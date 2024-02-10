6f499d9692297b04dce4e944da32672d01841d8f
gabriellefu <2234862823@qq.com>
2024-02-10 16:48:42 +0000

    the pass is changed

--- machop/chop/passes/graph/analysis/flop_estimator/FLOP_pass_ziyun.py
+++ machop/chop/passes/graph/analysis/flop_estimator/FLOP_pass_ziyun.py
@@ -11,7 +11,7 @@ def flop_pass_report(graph):
     for node in graph.fx_graph.nodes:
         module=node.meta['mase'].module
         #print(node.meta['mase'].parameters["common"]["mase_type"])
-        if (node.meta['mase'].parameters["common"]["mase_type"] = "placeholder") or (node.meta['mase'].parameters["common"]["mase_type"] = "output"):
+        if (node.meta['mase'].parameters["common"]["mase_type"] == "placeholder") or (node.meta['mase'].parameters["common"]["mase_type"] == "output"):
             flop_temp=0
         else:
             in_data=node.meta['mase'].parameters["common"]["args"]["data_in_0"]["value"]
@@ -26,23 +26,21 @@ def flop_pass_report(graph):
 
 
 def BitOP_pass_report(graph):
-    number_flop=0
+    number_BitOP=0
 
     for node in graph.fx_graph.nodes:
         module=node.meta['mase'].module
         #print(node.meta['mase'].parameters["common"]["mase_type"])
-        if (node.meta['mase'].parameters["common"]["mase_type"] is "placeholder") or (node.meta['mase'].parameters["common"]["mase_type"] is "output"):
+        if (node.meta['mase'].parameters["common"]["mase_type"] == "placeholder") or (node.meta['mase'].parameters["common"]["mase_type"] == "output"):
             flop_temp=0
         else:
             in_data=node.meta['mase'].parameters["common"]["args"]["data_in_0"]["value"]
             out_data=node.meta['mase'].parameters["common"]["results"]["data_out_0"]["value"]
-            flop_temp=calc_modules.calculate_modules(module, [in_data], [out_data])["computations"]   
+            flop_temp=calc_modules.calculate_modules(module, [in_data], [out_data])["bit_computations"]   
             #print("flop_temp in this layer=",flop_temp)  
         
-        number_flop+=flop_temp
+        number_BitOP+=flop_temp
         #print("number_flop=",number_flop)
 
-    return number_flop
+    return number_BitOP
 
-
-def count_bitop
